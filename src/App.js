5db8e9d6bd2a96b03612295288eca1d3b4a2ff71
JohnTagarian <pondkitnag@gmail.com>
2026-08-10 09:56:19 +0700

    add refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-// import { Content, Footer, Header } from "./func-components";
-// import Calendar from "./class-components";
-// import { Calculator2 } from "./calculator";
-import {EventData2} from './event-data'
-
+import RefsFunc from "./refs-func";
+  
 function App() {
-  return <EventData2 />;
+  return <RefsFunc />;
 }
 
 export default App;
