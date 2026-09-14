a656df30db5f9a4d8d214762988f253c23b58acf
JohnTagarian <pondkitnag@gmail.com>
2026-09-14 10:58:57 +0700

    add form-text-file.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Router3 from "./router3";
+import FormText from "./form-text";
+import FormSelectFile from "./form-select-file";
 
 export default function App() {
-  return <Router3/>;
+  return < FormSelectFile />;
 }
